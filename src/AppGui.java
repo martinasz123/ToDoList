1e207f6b88d6f59504d7aa4cded25c7c1cd8afc8
MartinSomogyi <martinasz123@gmail.com>
2025-03-10 19:01:18 +0000

    Frame completed

---
+++ src/AppGui.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class AppGui extends JFrame{
+    public AppGui(String user) {
+        super(user + "'s to do list");
+        setSize(450,650);
+    }
+
+}
---
+++ src/Launcher.java
@@ -0,0 +1,7 @@
+public class Launcher {
+    public static void main(String[] args)
+    {
+        String user = "Martin";
+        new AppGui(user).setVisible(true);
+    }
+}

02245851203b2247a31bee73f454d6af5a091fd0
MartinSomogyi <martinasz123@gmail.com>
2025-03-11 16:43:24 +0000

    Layout Fixed, more logic next

--- src/AppGui.java
+++ src/AppGui.java
@@ -1,24 +1,80 @@
 import javax.swing.*;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.LineBorder;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class AppGui extends JFrame{
+    private JPanel taskPanel;
     public AppGui(String user) {
         super(user + "'s to do list");
-        setSize(450,650);
+        setSize(500,500);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BoxLayout(getContentPane(),BoxLayout.Y_AXIS));
+        setLocationRelativeTo(null);
 
-        JPanel newTask = taskSection("laundry", 11.05);
-        add(newTask);
-        add(new JSeparator(SwingConstants.HORIZONTAL));
+        JPanel header = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        header.setMaximumSize(new Dimension(getWidth(), 2));
 
+        header.setBorder(new LineBorder(Color.black));
+        JButton taskBtn = taskBtn();
+        header.add(taskBtn);
+        add(header);
+
+        taskPanel = new JPanel();
+        taskPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+        add(taskPanel);
     }
 
-    public JPanel taskSection(String taskName, double completionDate){
+    public JDialog addTask(){
+        JDialog taskDetails = new JDialog(this, "Add Task", true);
+        taskDetails.setSize(500,100);
+        taskDetails.setLayout(new FlowLayout());
+        taskDetails.setLocationRelativeTo(null);
+
+        JTextField taskNameField = new JTextField("Task name: ");
+        taskDetails.add(taskNameField);
+
+        JLabel completion = new JLabel("Completion Date: ");
+
+        Date today = new Date();
+        JSpinner dateSpinner = new JSpinner(new SpinnerDateModel(today, null, null, Calendar.MONTH));
+        JSpinner.DateEditor date = new JSpinner.DateEditor(dateSpinner,"dd/MM/yy");
+        dateSpinner.setEditor(date);
+
+        JButton addTaskBtn = new JButton("Add Task");
+        addTaskBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String taskName = taskNameField.getText();
+                Date completionDate = (Date) dateSpinner.getValue();
+                taskSection(taskName,completionDate);
+                revalidate();
+            }
+        });
+
+        taskDetails.add(completion);
+        taskDetails.add(dateSpinner);
+        taskDetails.add(addTaskBtn);
+
+        return taskDetails;
+    }
+
+    public void taskSection(String taskName, Date completionDate){
         //Creating the panel for the task
         JPanel task = new JPanel();
+        task.setLayout(new BoxLayout(task, BoxLayout.X_AXIS));
+        task.setBorder(new BevelBorder(BevelBorder.RAISED));
+        SimpleDateFormat formatDate = new SimpleDateFormat("dd/MM/yyyy");
 
         //Creating the task fields
-        JLabel name = new JLabel("Task name: " + taskName);
-        JLabel date = new JLabel("Completion date: " + String.valueOf(completionDate));
-        JLabel completionName = new JLabel("Completed:");
+        JLabel name = new JLabel("<html><b>Task name:</b> " + taskName + " </html>");
+        JLabel date = new JLabel("<html><b>  Completion date:</b> " + String.valueOf(formatDate.format(completionDate)) + "</html>");
+        JLabel completionName = new JLabel("<html><b>Completed:</b></html>");
         JCheckBox completed = new JCheckBox();
 
         //adding the fields
@@ -27,6 +83,24 @@ public class AppGui extends JFrame{
         task.add(completionName);
         task.add(completed);
 
-        return task;
+        taskPanel.add(task,0);
+        taskPanel.revalidate();
+        taskPanel.repaint();
+
+    }
+
+    private JButton taskBtn(){
+        JButton addTaskBtn = new JButton("New Task");
+        addTaskBtn.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        addTaskBtn.setLayout(null);
+
+        addTaskBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JDialog newTask = addTask();
+                newTask.setVisible(true);
+            }
+        });
+        return addTaskBtn;
     }
 }
